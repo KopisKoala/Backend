5cfafed5af881ca6da247a298a1b0f3f3250603e
persi0815 <jina_8151010@naver.com>
2024-07-11 16:55:47 +0900

    Spring boot 초기 세팅

---
+++ src/main/java/kopis/k_backend/KBackendApplication.java
@@ -0,0 +1,13 @@
+package kopis.k_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kopis/k_backend/KBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kopis.k_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
