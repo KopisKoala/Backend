8a649c482a8069519a6eba1322221412ef8a0f20
persi0815 <jina_8151010@naver.com>
2024-07-13 21:17:24 +0900

    Feature: 로그인 위해 필요한 요청값들

---
+++ src/main/java/kopis/k_backend/user/dto/UserRequestDto.java
@@ -0,0 +1,40 @@
+package kopis.k_backend.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class UserRequestDto {
+    @Schema(description = "UserReqDto")
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserReqDto {
+
+        @Schema(description = "이메일")
+        private String email;
+
+        @Schema(description = "id(username)")
+        private String username;
+
+        @Schema(description = "social type")
+        private String provider;
+
+    }
+
+    @Schema(description = "UserNicknameReqDto")
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserNicknameReqDto {
+
+        @Schema(description = "nickname")
+        private String nickname;
+
+    }
+}
