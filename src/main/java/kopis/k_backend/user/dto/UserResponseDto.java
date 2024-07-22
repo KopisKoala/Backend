d56f765670d2c7f30fd44dd3fa0cb4696bdc5f0b
ldh6434 <ldh6434@naver.com>
2024-07-22 23:36:38 +0900

    refactor : SimpleuserDto 내 간소화

--- src/main/java/kopis/k_backend/user/dto/UserResponseDto.java
+++ src/main/java/kopis/k_backend/user/dto/UserResponseDto.java
@@ -2,54 +2,43 @@ package kopis.k_backend.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDto {
-
-    @Schema(description = "사용자 ID")
-    private Long id;
-
-    @Schema(description = "사용자 이름")
-    private String username;
-
-    @Schema(description = "닉네임")
-    private String nickname;
-
-    @Schema(description = "이메일")
-    private String email;
-
-    @Schema(description = "소셜 제공자")
-    private String provider;
-
-    @Schema(description = "주소")
-    private String address;
-
-    @Schema(description = "회원 등급")
-    private String userRank;
-
-    @Schema(description = "생성 일자")
-    private LocalDateTime createdAt;
-
-    @Schema(description = "수정 일자")
-    private LocalDateTime updatedAt;
 
     @Getter
     @Setter
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class UserUpdateDto {
-
+    public static class SimpleuserDto {
         @Schema(description = "닉네임")
         private String nickname;
 
+        @Schema(description = "이메일")
+        private String email;
+
         @Schema(description = "주소")
         private String address;
+
+        @Schema(description = "회원 등급")
+        private String userRank;
+
+        @Schema(description = "프로필 이미지")
+        private String profileImage;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserUpdateProfileImageDto {
+
+        @Schema(description = "프로필 이미지 변경")
+        private MultipartFile profileImage;
     }
 }
+
