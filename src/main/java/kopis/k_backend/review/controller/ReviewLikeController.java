c9556cff96299557a16027f3220e291612e82a16
양지원 <jina_8151010@naver.com>
2024-07-24 15:52:31 +0900

    Feature: 리뷰 좋아요 파일 생성

---
+++ src/main/java/kopis/k_backend/review/controller/ReviewLikeController.java
@@ -0,0 +1,15 @@
+package kopis.k_backend.review.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kopis.k_backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "리뷰 좋아요", description = "게시물 좋아요 관련 api 입니다.")
+@RestController
+@RequestMapping("/review/{review-id}/like")
+@RequiredArgsConstructor
+public class ReviewLikeController {
+    private final UserService userService;
+}
