e118d3b290bd75cc8815df6b953dc30aef591b38
양지원 <jina_8151010@naver.com>
2024-07-24 21:12:53 +0900

    refactor: 리뷰 정렬 마지막 기준은 생성시각(desc)으로 설정

--- src/main/java/kopis/k_backend/review/repository/ReviewRepository.java
+++ src/main/java/kopis/k_backend/review/repository/ReviewRepository.java
@@ -15,22 +15,34 @@ import kopis.k_backend.review.domain.Review;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("SELECT r FROM Review r WHERE (r.performance = :performance) ORDER BY " +
             "CASE WHEN :way = 'recent' THEN r.createdAt END DESC, " +
+
             "CASE WHEN :way = 'like' THEN r.likeCount END DESC, " +
             "CASE WHEN :way = 'like' THEN r.createdAt END DESC, " +
+
             "CASE WHEN :way = 'desc' THEN r.performanceRatings END DESC, " +
             "CASE WHEN :way = 'desc' THEN r.likeCount END DESC, " +
+            "CASE WHEN :way = 'desc' THEN r.createdAt END DESC, " +
+
             "CASE WHEN :way = 'asc' THEN r.performanceRatings END ASC, " +
-            "CASE WHEN :way = 'asc' THEN r.likeCount END DESC")
+            "CASE WHEN :way = 'asc' THEN r.likeCount END DESC, "+
+            "CASE WHEN :way = 'asc' THEN r.createdAt END DESC")
+
     Slice<Review> findReviewByPerformanceAndWay(@Param("performance")Performance performance, @Param("way") String way, Pageable pageable);
 
     @Query("SELECT r FROM Review r WHERE (r.pair = :pair) ORDER BY " +
             "CASE WHEN :way = 'recent' THEN r.createdAt END DESC, " +
+
             "CASE WHEN :way = 'like' THEN r.likeCount END DESC, " +
             "CASE WHEN :way = 'like' THEN r.createdAt END DESC, " +
-            "CASE WHEN :way = 'desc' THEN r.performanceRatings END DESC, " +
+
+            "CASE WHEN :way = 'desc' THEN r.pairRatings END DESC, " +
             "CASE WHEN :way = 'desc' THEN r.likeCount END DESC, " +
-            "CASE WHEN :way = 'asc' THEN r.performanceRatings END ASC, " +
-            "CASE WHEN :way = 'asc' THEN r.likeCount END DESC")
+            "CASE WHEN :way = 'desc' THEN r.createdAt END DESC, " +
+
+            "CASE WHEN :way = 'asc' THEN r.pairRatings END ASC, " +
+            "CASE WHEN :way = 'asc' THEN r.likeCount END DESC, "+
+            "CASE WHEN :way = 'asc' THEN r.createdAt END DESC")
+
     Slice<Review> findReviewByPairAndWay(@Param("pair") Pair pair, @Param("way") String way, Pageable pageable);
 
 }
