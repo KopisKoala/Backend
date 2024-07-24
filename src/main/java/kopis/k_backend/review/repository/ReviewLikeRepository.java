9048e749fdca5d889f619aed89117065b5848cd4
양지원 <jina_8151010@naver.com>
2024-07-25 05:10:57 +0900

    Feature: 리뷰 좋아요 생성, 삭제 기능

--- src/main/java/kopis/k_backend/review/repository/ReviewLikeRepository.java
+++ src/main/java/kopis/k_backend/review/repository/ReviewLikeRepository.java
@@ -1,8 +1,14 @@
 package kopis.k_backend.review.repository;
 
+import kopis.k_backend.review.domain.Review;
 import kopis.k_backend.review.domain.ReviewLike;
+import kopis.k_backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
+
+    Optional<ReviewLike>findByUserAndReview(User user, Review review);
 
 }
