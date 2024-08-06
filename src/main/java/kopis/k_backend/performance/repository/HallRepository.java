2a53ac875fbb9f044bbad98da0c6786874575ab5
양지원 <jina_8151010@naver.com>
2024-08-06 18:36:09 +0900

    Feature: 공연장 repository

--- src/main/java/kopis/k_backend/performance/repository/HallRepository.java
+++ src/main/java/kopis/k_backend/performance/repository/HallRepository.java
@@ -2,6 +2,14 @@ package kopis.k_backend.performance.repository;
 
 import kopis.k_backend.performance.domain.Hall;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Optional;
 
 public interface HallRepository extends JpaRepository<Hall, Long> {
+    Optional<Hall> findByKopisHallId(String kopisHallId);
+
+    @Query("SELECT h.kopisHallId FROM Hall h WHERE h.kopisHallId IS NOT NULL")
+    List<String> findAllKopisHallIds();
+
 }
