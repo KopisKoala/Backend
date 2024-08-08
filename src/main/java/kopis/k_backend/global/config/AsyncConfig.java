9bbee6c6330058e705bc276e8d4ebe9840d7df7f
양지원 <jina_8151010@naver.com>
2024-08-08 23:19:55 +0900

    refactor: 스레드 풀 제한 설정

---
+++ src/main/java/kopis/k_backend/global/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package kopis.k_backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("KopisPerfThread-");
+        executor.initialize();
+        return executor;
+    }
+}
