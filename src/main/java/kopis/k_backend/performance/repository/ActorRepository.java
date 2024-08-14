be2b22bc91d7362f4462bde5b638d2f690448e05
양지원 <jina_8151010@naver.com>
2024-08-15 01:25:07 +0900

    Feature: 배우 관련 레포지토리

--- src/main/java/kopis/k_backend/performance/repository/ActorRepository.java
+++ src/main/java/kopis/k_backend/performance/repository/ActorRepository.java
@@ -3,6 +3,8 @@ package kopis.k_backend.performance.repository;
 import kopis.k_backend.performance.domain.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActorRepository extends JpaRepository<Actor, Long> {
+import java.util.Optional;
 
+public interface ActorRepository extends JpaRepository<Actor, Long> {
+    Optional<Actor> findByActorNameAndActorProfile(String actorName, String actorProfile);
 }
