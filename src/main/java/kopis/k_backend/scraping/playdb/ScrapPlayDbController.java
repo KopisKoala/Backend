7cc996d4e034932e296dbb6cbf00ac9e9630976b
양지원 <jina_8151010@naver.com>
2024-08-15 01:51:27 +0900

    Feature: 스크래핑 사용하여 playdb에서 배우/배역 정보 가져와서 db에 저장하기

---
+++ src/main/java/kopis/k_backend/scraping/playdb/ScrapPlayDbController.java
@@ -0,0 +1,35 @@
+package kopis.k_backend.scraping.playdb;
+
+import kopis.k_backend.global.api_payload.ApiResponse;
+import kopis.k_backend.global.api_payload.SuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/scrape/playdb")
+public class ScrapPlayDbController {
+    private final ScrapPlayDbService scrapPlayDbService;
+
+    @PostMapping("/all")
+    public ApiResponse<String> scrapeAndSaveActorsByOne() {
+        List<Long> performanceIds = scrapPlayDbService.findPerformancesWithNonPlayDBCast();
+
+        for(Long id : performanceIds){
+            scrapPlayDbService.scrapeAndSaveActors(id);
+        }
+        return ApiResponse.onSuccess(SuccessCode.SCRAP_PERF_PUT_ACTOR_SUCCESS, "PLAYDB의 공연 배우 정보를 DB에 넣었습니다.");
+    }
+
+    @PostMapping("/one-by-one")
+    public ApiResponse<String> scrapeAndSaveActorsByOnce(@RequestParam("perfId") Long performanceId) {
+        scrapPlayDbService.scrapeAndSaveActors(performanceId);
+        return ApiResponse.onSuccess(SuccessCode.SCRAP_PERF_PUT_ACTOR_SUCCESS, "PLAYDB의 공연 배우 정보를 DB에 넣었습니다.");
+    }
+
+}
