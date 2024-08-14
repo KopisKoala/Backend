c7bc66a65ccdf1268da9f1e48b82dd36919da7b6
양지원 <jina_8151010@naver.com>
2024-08-15 01:24:13 +0900

    스크래핑 성공 코드

--- src/main/java/kopis/k_backend/global/api_payload/SuccessCode.java
+++ src/main/java/kopis/k_backend/global/api_payload/SuccessCode.java
@@ -39,6 +39,9 @@ public enum SuccessCode implements BaseCode {
     DB_PERF_LIST_SUCCESS(HttpStatus.OK, "DB_PERF_2013", "공연 리스트를 DB에 넣었습니다."),
     DB_PERF_DETAIL_SUCCESS(HttpStatus.OK, "DB_PERF_2014", "공연 정보를 DB에 넣었습니다."),
 
+    // Scrap
+    SCRAP_PERF_PUT_ACTOR_SUCCESS(HttpStatus.OK, "SCRAP_PERF_2011", "PLAYDB의 공연 배우 정보를 DB에 넣었습니다."),
+
     // Open AI
     OPEN_AI_API_SUCCESSS(HttpStatus.OK, "OPEN_2001", "GPT API 요청이 완료되었습니다."),
     OPEN_AI_PERFORMANCE_REVIEW_SUMMARY(HttpStatus.OK, "OPEN_2002", "공연 별 REVIEW 요약이 완료되었습니다."),
