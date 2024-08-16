d029b974f178475448a26a7e1a595b3eab500d6e
양지원 <jina_8151010@naver.com>
2024-08-16 14:29:57 +0900

    안쓰는 import 제거

--- src/main/java/kopis/k_backend/performance/repository/PerformancePopularRepositoryPlay.java
+++ src/main/java/kopis/k_backend/performance/repository/PerformancePopularRepositoryPlay.java
@@ -1,6 +1,5 @@
 package kopis.k_backend.performance.repository;
 
-import kopis.k_backend.performance.domain.PerformancePopularMusical;
 import kopis.k_backend.performance.domain.PerformancePopularPlay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
