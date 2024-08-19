7411e178dbfff8cac8b7b0ed76ff631e54f78974
양지원 <jina_8151010@naver.com>
2024-08-19 17:22:14 +0900

    해당 로직도 job entity로 상황 볼 수 있도록

--- src/main/java/kopis/k_backend/performance/service/PerformanceRankingService.java
+++ src/main/java/kopis/k_backend/performance/service/PerformanceRankingService.java
@@ -1,13 +1,15 @@
 package kopis.k_backend.performance.service;
 
+import kopis.k_backend.job.Job;
+import kopis.k_backend.job.JobRepository;
 import kopis.k_backend.performance.domain.*;
 import kopis.k_backend.performance.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -20,12 +22,20 @@ public class PerformanceRankingService {
     private final PerformancePopularPlayRepository performancePopularPlayRepository;
     private final PerformanceAdvertiseRepository performanceAdvertiseRepository;
     private final PerformanceAttractRepository performanceAttractRepository;
+    private final JobRepository jobRepository;
 
-    @Scheduled(cron = "30 23 0 * * *", zone = "Asia/Seoul") // 매일 밤 11시 30분
+    @Scheduled(cron = "0 30 23 * * *", zone = "Asia/Seoul") // 매일 밤 11시 30분
     public void extractAttractPerformance() {
-        LocalDate today = LocalDate.now().plusDays(1);
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter jobFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm");
+        String jobId = today.format(jobFormatter);
+        String jobType = "PERFORMANCE_ATTRACT_UPDATE";
+        Job jobEntity = new Job(jobId, "-", "IN_PROGRESS", jobType);
+        jobRepository.save(jobEntity);
+
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        String formattedDate = today.format(formatter);  // 내일 날짜를 "yyyy.MM.dd" 형식으로 변환
+        String formattedDate = tomorrow.format(formatter);  // 내일 날짜를 "yyyy.MM.dd" 형식으로 변환
 
         List<Performance> performances = performanceRepository.findTop10ByMinReviewsOrderByRatingAndReviewCount(5L);
 
@@ -40,6 +50,10 @@ public class PerformanceRankingService {
                     .build();
             performanceAttractRepository.save(performanceAttract);
         }
+
+        LocalDateTime now = LocalDateTime.now();
+        jobEntity.setStatus("COMPLETED"); jobEntity.setEnd(now.format(jobFormatter));
+        jobRepository.save(jobEntity); // 완료
     }
 
     public List<PerformancePopularMusical> popularMusicalList(String date){
