9c759e9155aa4bca8d34cb6880113445eaf5e9b9
양지원 <jina_8151010@naver.com>
2024-08-19 14:20:58 +0900

    refactor: 페어에 배우 이름이 아닌 id로 저장

--- src/main/java/kopis/k_backend/pair/converter/PairConverter.java
+++ src/main/java/kopis/k_backend/pair/converter/PairConverter.java
@@ -1,7 +1,11 @@
 package kopis.k_backend.pair.converter;
 
+import kopis.k_backend.global.api_payload.ErrorCode;
+import kopis.k_backend.global.exception.GeneralException;
 import kopis.k_backend.pair.domain.Pair;
 import kopis.k_backend.pair.dto.PairResponseDto;
+import kopis.k_backend.performance.domain.Actor;
+import kopis.k_backend.performance.repository.ActorRepository;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -10,11 +14,19 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class PairConverter {
 
+    private static ActorRepository actorRepository;
+
     public static PairResponseDto.SimplePairResDto simplePairResDto(Pair pair){
+        Actor actor_1 = actorRepository.findById(pair.getActor1())
+                .orElseThrow(() -> GeneralException.of(ErrorCode.ACTOR_NOT_FOUND));
+
+        Actor actor_2 = actorRepository.findById(pair.getActor2())
+                .orElseThrow(() -> GeneralException.of(ErrorCode.ACTOR_NOT_FOUND));
+
         return PairResponseDto.SimplePairResDto.builder()
                 .pairId(pair.getId())
-                .actor1(pair.getActor1Name())
-                .actor2(pair.getActor2Name())
+                .actor1(actor_1.getActorName())
+                .actor2(actor_2.getActorName())
                 .build();
     }
 
